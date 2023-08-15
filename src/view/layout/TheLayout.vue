1aad4c05f6bc40cc630850e617d0822f83175bea
Server <matink1490@gmail.com>
2023-08-15 16:09:44 -0700

    Delete socket io

--- src/view/layout/TheLayout.vue
+++ src/view/layout/TheLayout.vue
@@ -21,56 +21,10 @@ import TheLayoutContent from './TheLayoutContent.vue';
 import TheLayoutFab from './TheLayoutFab.vue';
 import TheLayoutToTopFab from './TheLayoutToTopFab.vue';
 import TheLayoutFooter from './TheLayoutFooter.vue';
-import Vue from "vue";
-import {io} from "socket.io-client";
 
 export default {
     async created() {
         Notification.requestPermission().then()
-        const socketIO = new io(Vue.prototype.serverAddress, {
-            path: "/api/events",
-            query: {
-                'Authorization': sessionStorage.getItem('Authorization')
-            }
-        })
-        this.$store.subscribeAction(({type}) => {
-            if (type === 'stopSound') {
-                this.audio.pause();
-                this.audio.currentTime = 0;
-                this.$store.commit('SET_SOUND_PLAY_STATE', false)
-            }
-        })
-
-        socketIO.connect();
-        socketIO.on('connection', () => {
-        })
-        Vue.prototype.socketIO = socketIO;
-        socketIO.on('notification', (data) => {
-            if (!!data.soundNotification) {
-                this.$store.commit('SET_SOUND_PLAY_STATE', true)
-                const source = this.serverAddress + data.soundAlarm;
-                this.audio = document.getElementById('audio');
-                this.audio.src = source;
-                this.audio.muted = false;
-                this.audio.play().then(() => {
-                }).catch((e) => {
-                    console.error(e)
-                })
-                this.audio.onended = () => {
-                    this.$store.commit('SET_SOUND_PLAY_STATE', false);
-                }
-            }
-            if (!!data.browserNotification) {
-                const notificationData = data.browserNotification;
-                Notification.requestPermission().then((result) => {
-                    new Notification(notificationData.title, {
-                        body: notificationData.content,
-                        image: this.baseUrl + notificationData.image,
-                        dir: "rtl",
-                    })
-                });
-            }
-        });
         await this.$store.dispatch('initSessionId')
         const [err, data] = await this.to(this.http.get(this.serverAddress + `/api/auth/currentProfile`));
         if (!err) {
