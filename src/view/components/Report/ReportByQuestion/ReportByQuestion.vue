11967ca3262966ee25f16a2f2bb8bf18a8e9ad12
Server <matink1490@gmail.com>
2023-08-16 10:50:28 -0700

    Handle bugs

--- src/view/components/Report/ReportByQuestion/ReportByQuestion.vue
+++ src/view/components/Report/ReportByQuestion/ReportByQuestion.vue
@@ -5,45 +5,56 @@
                 گزارش بر اساس سؤالات
             </v-card-title>
             <v-card-text>
-                <v-data-table
-                        :items="table.contents"
-                        :headers="table.headers">
-                    <template v-slot:item.row="{ item , index}">
-                        <div> {{ table.contents.indexOf(item) + 1 }}</div>
-                    </template>
-                    <template v-slot:item.averageValue="{ item , index}">
-                        {{ item.averageValue + (!!item.likertLabelTitle ? ` (${item.likertLabelTitle})` : '')}}
-                    </template>
+                <div class="row">
+                    <div class="col-12">
+                        <v-data-table
+                                :items="table.contents"
+                                :headers="table.headers">
+                            <template v-slot:item.row="{ item , index}">
+                                <div> {{ table.contents.indexOf(item) + 1 }}</div>
+                            </template>
+                            <template v-slot:item.averageValue="{ item , index}">
+                                {{ item.averageValue + (!!item.likertLabelTitle ? ` (${item.likertLabelTitle})` : '') }}
+                            </template>
 
-                    <template v-slot:item.actions="{ item , index}">
-                        <div class="d-inline-flex">
-                            <v-tooltip
-                                v-if="checkPermission('reports_by_user')"
-                                color="blue"
-                                bottom>
-                                <template v-slot:activator="{ on, attrs }">
-                                    <v-btn
-                                        v-bind="attrs"
-                                        v-on="on"
-                                        @click="seeDetails(item)"
-                                        class="sqaure-btn white--text mx-1"
-                                        color="primary">
-                                        <v-icon>
-                                            mdi-eye
-                                        </v-icon>
-                                    </v-btn>
-                                </template>
-                                {{ $t('ui.details') }}
-                            </v-tooltip>
-                        </div>
-                    </template>
-                </v-data-table>
+                            <template v-slot:item.actions="{ item , index}">
+                                <div class="d-inline-flex">
+                                    <v-tooltip
+                                            v-if="checkPermission('reports_by_user')"
+                                            color="blue"
+                                            bottom>
+                                        <template v-slot:activator="{ on, attrs }">
+                                            <v-btn
+                                                    v-bind="attrs"
+                                                    v-on="on"
+                                                    @click="seeDetails(item)"
+                                                    class="sqaure-btn white--text mx-1"
+                                                    color="primary">
+                                                <v-icon>
+                                                    mdi-eye
+                                                </v-icon>
+                                            </v-btn>
+                                        </template>
+                                        {{ $t('ui.details') }}
+                                    </v-tooltip>
+                                </div>
+                            </template>
+                        </v-data-table>
+                    </div>
+                    <!--                    <div class="col-6">-->
+                    <!--                        <div dir="ltr">-->
+                    <!--                            <div id="sales_chart"></div>-->
+                    <!--                        </div>-->
+                    <!--                    </div>-->
+                </div>
             </v-card-text>
         </v-card>
     </v-container>
 </template>
 
 <script>
+import zc, {zingchart} from 'zingchart/es6.js';
+import 'zingchart/modules-es6/zingchart-pareto.min.js';
 
 export default {
     name: "ReportByQuestion",
@@ -64,6 +75,24 @@ export default {
         const [err, data] = await this.to(this.http.get(`/admin/report/by-question`));
         if (!err) {
             this.table.contents = data;
+
+
+            // zc.render({
+            //     id: 'sales_chart',
+            //     data: {
+            //         type: "bar",
+            //         title: {text: "فراوانی بیشترین فروش دوره‌ها در 30 روز گذشته"},
+            //         series: data.map(f => {
+            //             return {
+            //                 text: 'f.title',
+            //                 values: [37, 51, 0]
+            //             }
+            //         }),
+            //         scaleX: {
+            //             labels: data.map(f => f.title)
+            //         },
+            //     }
+            // });
         }
     }
 
